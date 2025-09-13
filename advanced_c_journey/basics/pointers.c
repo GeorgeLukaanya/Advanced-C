daf725d7a1105be9988614053ba908c811bad676
GeorgeLukaanya <georgelukaanya348@gmail.com>
2025-09-13 17:23:15 +0300

    This is step one of the journey

---
+++ advanced_c_journey/basics/pointers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x = 10;
+    int *ptr = &x;
+
+    printf("Value of x: %d\n", x);
+    printf("Value at pointer p: %d\n", *p);
+    printf("Address of x: %p\n", (void*)&x);
+
+}
